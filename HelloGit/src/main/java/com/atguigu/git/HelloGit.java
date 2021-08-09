0573ae43cf4712feebb1fe03ea27bc4a61fb6a2c
Loser-001 <1669279462@qq.com>
2021-08-09 18:23:45 +0800

    2021/8/9 maven_git_idea 测试添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author Jerry
+ * @create 2021-08-09 18:11
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!!!");
+    }
+}
